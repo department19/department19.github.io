55bb3b1cd36ffb905383832f4e852f1d7f46a3d0
Jack Lam <jackktlam@gmail.com>
2023-10-08 22:10:34 +0100

    animation working at a basic level

--- app.js
+++ app.js
@@ -12,10 +12,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function prepAnimation() {
     allElements.forEach((element) => {
-      element.classList.add("init-animation");
+      element.classList.add("init-animate");
     });
-    // allElements.classList.add("init-animation");
   }
 
   prepAnimation();
+
+  function triggerAnimation() {
+    console.log("trigger");
+    allElements.forEach(element => {
+      let triggerThreshold = 150;
+      let windowHeight = window.innerHeight;
+      // let elementTop = element.getBoundingClientRect();
+      // console.log(elementTop);
+      
+      if (elementTop < windowHeight - triggerThreshold) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", function() {
+    triggerAnimation();
+  });
+
+  const scrollObserver = new IntersectionObserver ((component) => {
+    console.log(component);
+    component.forEach((part) => {
+      if (part.isIntersecting) {
+        part.target.classList.add("active");
+        // part.target.classList.remove("init-animate");
+      } else {
+        part.target.classList.remove("active");
+        // part.target.classList.add("init-animate");
+      }
+    });
+  });
+
+  allElements.forEach(element => {
+    scrollObserver.observe(element);
+  });
+
 });
