154a8fb3c2ac30d5e9da921f81e3a17e6bd718ba
Jack L <jackktlam@gmail.com>
2023-10-08 16:27:41 +0100

    animation commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
   //   Navbar functionality
   const navBar = document.getElementById("navbar");
 
   function togglenavbar() {
     navBar.classList.toggle("nav-active");
   }
+
+
+  // initial animation
+  const allElements = document.querySelectorAll("section > *");
+
+  function prepAnimation() {
+    allElements.forEach((element) => {
+      element.classList.add("init-animation");
+    });
+    // allElements.classList.add("init-animation");
+  }
+
+  prepAnimation();
 });
